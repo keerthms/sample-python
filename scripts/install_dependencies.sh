b8064773efdcd004de4bf352239199fea7744a26
Ubuntu <ubuntu@ip-10-0-15-103.ec2.internal>
2024-09-20 10:31:23 +0000

    update script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 cd /home/ubuntu/python-app
+#!/bin/bash
+
+# Install pip if not installed
+if ! command -v pip3 &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y python3-pip
+fi
+
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
