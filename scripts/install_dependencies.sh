4442afc18ff62285dbe0414683293a7c472e5a4f
keerthms <167199283+keerthms@users.noreply.github.com>
2024-09-20 16:12:03 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu/python-app
-#!/bin/bash
+code_path="${/home/ubuntu/python-app}"
+cd ${code_path}
 
 # Install pip if not installed
 if ! command -v pip3 &> /dev/null; then
@@ -10,5 +10,7 @@ fi
 
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
-
+cd "${code_path}/venv/bin/"
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip install -r "${code_path}/requirements.txt"
